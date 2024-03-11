4cdb6ca5288beb7feb83b28b9c6fa5fdd071c8cf
swaff-1931918 <swaff@uw.edu>
2024-03-10 18:50:57 -0700

    Refactors making the guess

--- AutoSolvle.js
+++ AutoSolvle.js
@@ -2,26 +2,34 @@
  * Class to accomplish Goal 2: auto play the online Wordle with no need for user input
  */
 'use strict';
-
+(async () => {
 const { default: puppeteer } = require("puppeteer");
+const fs = require('node:fs');
 
 let words;
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
-const ENTER = document.querySelectorAll('.Key-module_key__kchQI')[19];
-
+/**
+ * Initializes a new Solvle. Loads in a dictionary of words and begins.
+ */
 function init() {
+    console.log('made to init');
     loadWords();
     solve();
 }
 
-
+/**
+ * Begins solving the Wordle!
+ */
 async function solve() {
     let guess;
     let solved = false;
     let attempts = 0;
     await page.goto('https://www.nytimes.com/games/wordle/index.html');
+    await page.click('button[data-testid=Play]');
+    //await page.click('.Modal-module_closeIcon__TcEKb');
+    console.log('made it to NYT');
     // set screen size?
     while (!solved && attempts < 6) {
         // Need to get a word to guess
@@ -37,15 +45,40 @@ async function solve() {
         attempts += 1;
     }
     // TODO: make it save to a .log file with the date, attempts, outcome
+    await browser.close();
+    console.log('Game complete. Number of guesses: ' + attempts + '.\n');
+    if (solved) {
+        console.log('VICTORY!');
+    } else {
+        console.log('DEFEAT');
+    }
 }
 
+/**
+ * Checks how good our guess is, removes words from words that don't fit the "regex"
+ * @param {Integer} attempt The number of guesses so far
+ * @returns {boolean} Whether the Wordle has been solved
+ */
 async function checkGuess(attempt) {
-    // need to make sure that I also update the words
-    var content = await page.content();
-    var rowOfLetters = content.querySelectorAll('.Row-module_row__pwpBq')[attempt].querySelectorAll('.Tile-module_tile__UWEHN');
+    console.log('checking guess');
+    let allTiles = await page.$$('.Tile-module_tile__UWEHN');
+    console.log(allTiles.length);
+    let rowOfLetters = allTiles.slice(attempt * 5, attempt * 5 + 5);
+    /*
+    Could just query for all 30 boxes and then only look at window of 5 based on attempt * 5
+    */
+    //let rowOfLetters = gameBoard.childNodes[attempt];
+    // then I need to manipulate with [attempt] and then further selcting down where
+    // .querySelectorAll('.Tile-module_tile__UWEHN')
+    let numCorrect = 0;
     for (let i = 0; i < 5; i++) {
+        console.log(rowOfLetters[i].classList);
+        return;
         let letter = rowOfLetters[i].textContent;
-        let stateOfLetter = rowOfLetters[i].dataset.state; // absent is gray, present is yellow, correct is green
+        let stateOfLetter = rowOfLetters[i].attributes['data-state'].textContent; // absent is gray, present is yellow, correct is green
+        if (stateOfLetter == 'correct') {
+            numCorrect += 1;
+        }
         for (let j = 0; j < words.size(); j++) {
             switch (stateOfLetter) {
                 case 'correct': // guessed letter is in final position
@@ -68,8 +101,8 @@ async function checkGuess(attempt) {
             }
         }
     }
-    updateWords();
-    return true;
+    console.log('Number of greens: ' + numCorrect);
+    return numCorrect == 5;
 }
 
 /**
@@ -78,12 +111,10 @@ async function checkGuess(attempt) {
  */
 async function makeGuess(guess) {
     // use the .click() attr of the letter on the keyboard I'm looking for
-    for (let i = 0; i < 5; i++) {
-        // prolly need to set some delay?
-        var key = 'button[data-key=' + guess[i] + ']';
-        await page.click(key);
-    }
-    await page.click(ENTER);
+    console.log('Attempting to guess: ' + guess);
+    // Refactored code:
+    await page.type('#wordle-app-game', guess, {delay: 100});
+    await page.keyboard.press('Enter');
 }
 
 /**
@@ -98,12 +129,15 @@ function getGuess() {
  * Read in each word from words.txt into the array words
  */
 function loadWords() {
-    fetch('words.txt')
-        .then((res) => res.text())
-        .then((text) => {
+    fs.readFile('words.txt', 'utf-8', (e, text) => {
+        if (e) {
+            console.error(err);
+        } else {
             words = text.split('\n');
-        })
-        .catch((e) => console.error(e));
+        }
+    })
 }
 
 init();
+
+})();
