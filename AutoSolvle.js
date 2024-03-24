dc9d728bac1d45a1a66cf5d68aaac0a3379a15dc
swaff-1931918 <swaff@uw.edu>
2024-03-24 16:48:57 -0700

    Adds a 2s delay so the tiles can flip after a guess

--- AutoSolvle.js
+++ AutoSolvle.js
@@ -27,14 +27,16 @@ async function solve() {
     let solved = false;
     let attempts = 0;
     await page.goto('https://www.nytimes.com/games/wordle/index.html');
+    await page.waitForSelector('button[data-testid=Play]');
     await page.click('button[data-testid=Play]');
-    //await page.click('.Modal-module_closeIcon__TcEKb');
     console.log('made it to NYT');
     // set screen size?
     while (!solved && attempts < 6) {
         // Need to get a word to guess
         guess = getGuess();
-
+        if (guess === null) {
+            break;
+        }
         // Need to post the word to NYT
         await makeGuess(guess);
 
@@ -61,46 +63,64 @@ async function solve() {
  */
 async function checkGuess(attempt) {
     console.log('checking guess');
-    let allTiles = await page.$$('.Tile-module_tile__UWEHN');
-    console.log(allTiles.length);
-    let rowOfLetters = allTiles.slice(attempt * 5, attempt * 5 + 5);
+    // TODO: THIS IS THE BOTTLENECK
+    let allTilesLetters = await page.$$eval('.Tile-module_tile__UWEHN', (tiles) => {
+        return tiles.map(tile => tile.textContent);
+    });
+    let allTilesStatus = await page.$$eval('.Tile-module_tile__UWEHN', (tiles) => {
+        return tiles.map(tile => tile.attributes['data-state'].textContent);
+    });
+    console.log(allTilesLetters.length);
+    console.log('dictionary of words before is size: %d', words.length);
+    let rowOfLetters = allTilesLetters.slice(attempt * 5, attempt * 5 + 5);
+    let rowOfStates = allTilesStatus.slice(attempt * 5, attempt * 5 + 5);
+    let tempWords = [];
+    console.log(rowOfStates);
     /*
-    Could just query for all 30 boxes and then only look at window of 5 based on attempt * 5
+        Prolly going to refactor my finding algo:
+        Criteria for keeping words:
+        guess = 'steak', word = words[j]
+        if state[i] is green, word[i] HAS to equal guess[i]
+        if state[i] is yellow, keep words that 1) MUST contain guess[i] 2) guess[i] != word[i] 3) word[k] == guess[i], state[k] != green
+        if state[i] is gray, keep words that DO NOT contain guess[i]
     */
-    //let rowOfLetters = gameBoard.childNodes[attempt];
-    // then I need to manipulate with [attempt] and then further selcting down where
-    // .querySelectorAll('.Tile-module_tile__UWEHN')
-    let numCorrect = 0;
-    for (let i = 0; i < 5; i++) {
-        console.log(rowOfLetters[i].classList);
-        return;
-        let letter = rowOfLetters[i].textContent;
-        let stateOfLetter = rowOfLetters[i].attributes['data-state'].textContent; // absent is gray, present is yellow, correct is green
-        if (stateOfLetter == 'correct') {
-            numCorrect += 1;
-        }
-        for (let j = 0; j < words.size(); j++) {
-            switch (stateOfLetter) {
-                case 'correct': // guessed letter is in final position
-                    if (words[j][i] != letter) {
-                        words.remove(j);
-                        j -= 1;
+    console.log(rowOfLetters[0]);
+    for (let i = 0; i < words.length; i++) {
+        let keep = true;
+        let j = 0;
+        while (keep && j < 5) {
+            switch (rowOfStates[j]) {
+                case 'correct': // green
+                    if (words[i][j] != rowOfLetters[j]) {
+                        keep = false;
                     }
                     break;
-                case 'present': // remove words that dont have the letter anywhere in it
-                    if (!words[j].includes(letter)) {
-                        words.remove(j);
-                        j -= 1;
+                case 'present': // yellow
+                    if (words[i][j] == rowOfLetters[j]) {
+                        keep = false;
+                    } else if (!words[i].includes(rowOfLetters[j])) {
+                        keep = false;
                     }
                     break;
-                default: // guessed letter isnt in the word
-                    if (words[j].includes(letter)) {
-                        words.remove(j);
-                        j -= 1;
+                case 'absent': // gray
+                    if (words[i].includes(rowOfLetters[j])) {
+                        keep = false;
                     }
             }
+            j += 1;
+        }
+        if (keep) {
+            tempWords.push(words[i]);
         }
     }
+    let numCorrect = 0;
+    for (let i = 0; i < 5; i++) {
+        if (rowOfStates[i] == 'correct') {
+            numCorrect += 1;
+        }
+    }
+    words = tempWords;
+    console.log('dictionary of tempWords before is size: %d', tempWords.length);
     console.log('Number of greens: ' + numCorrect);
     return numCorrect == 5;
 }
@@ -110,11 +130,14 @@ async function checkGuess(attempt) {
  * @param {String} guess 5 letter word all lower case. The word to be guessed
  */
 async function makeGuess(guess) {
-    // use the .click() attr of the letter on the keyboard I'm looking for
     console.log('Attempting to guess: ' + guess);
     // Refactored code:
-    await page.type('#wordle-app-game', guess, {delay: 100});
-    await page.keyboard.press('Enter');
+    await page.type('#wordle-app-game', guess)
+                .then(async () => {
+                    await (await page.$('button[data-key="↵"]')).press('Enter');
+                });
+    await new Promise((res, rej) => setTimeout(res, 2000));
+    // THE TILES TAKE 100MS TO FLIP AFTER A GUESS! SO NEED TO WAIT .5MS BEFORE CHECKING!
 }
 
 /**
@@ -122,7 +145,10 @@ async function makeGuess(guess) {
  * @returns first valid word
  */
 function getGuess() {
-    return words[0];
+    if (words.length > 0) {
+        return words[0];
+    }
+   return null;
 }
 
 /**
